6b10dad2626e78de51db3e2634b3566f4e84b548
lxsang <xsang.le@gmail.com>
2019-12-21 00:32:48 +0100

    add ws, not tested

--- src/antd.rs
+++ src/antd.rs
@@ -10,14 +10,14 @@ use std::fmt::{Arguments,Write};
 use std::fs::File;
 use std::io::BufReader;
 
+const MAX_BUF_SIZE: usize = 1024;
+
 static mut __PLUGIN__: PluginHeader = PluginHeader{
     name: std::ptr::null(),
     dbpath: std::ptr::null(),
-    htdocs: std::ptr::null(),
+    tmpdir: std::ptr::null(),
     pdir: std::ptr::null(),
-    sport: 0,
-    raw_body: 0,
-    usessl: 0
+    raw_body: 0
 };
 
 macro_rules! cstr {
@@ -34,24 +34,29 @@ pub struct PluginHeader
 {
     name: *const c_char,
     dbpath: *const c_char,
-    htdocs: * const c_char,
+    tmpdir: * const c_char,
     pdir: *const c_char,
-	sport: u32,
-    raw_body: u32,
-    usessl: u32
+    raw_body: u32
 }
 
 
 #[repr(C)]
-pub struct Config
+pub struct PortConfig
 {
     port: u32,
+    usessl: i32,
+    htdocs: *const c_char,
+    sock: i32,
+    rules: *const c_void
+}
+
+#[repr(C)]
+pub struct Config
+{
     plugins_dir: *const c_char,
     plugins_ext: *const c_char,
     db_path: *const c_char,
-    htdocs: *const c_char,
     tmpdir: *const c_char,
-    rules: *const c_void,
     handlers: *const c_void,
     backlog: u32,
     maxcon: u32,
@@ -59,9 +64,12 @@ pub struct Config
     n_workers: u32,
     errorfp: *const c_void,
     logfp: * const c_void,
-    usessl: u32,
+    enable_ssl: u32,
     sslcert: *const c_char,
-    sslkey:* const c_char
+    sslkey:* const c_char,
+    ssl_cipher: *const c_char,
+    mimes: *const Dictionary,
+    ports: *const Dictionary
 }
 
 
@@ -72,7 +80,8 @@ pub struct Client
     ssl: *const c_void,
     ip: *const c_char,
     status: u32,
-    last_io: libc::time_t
+    last_io: libc::time_t,
+    port_config: *const PortConfig
 }
 
 //impl Client {
@@ -117,18 +126,78 @@ pub struct Request
     data: *const Dictionary
 }
 
+pub enum WSStatus{
+    OK,
+    CLOSED,
+    ERR,
+    NOT_WS
+}
+
 impl Request {
 
     pub fn get_data(&self) -> RequestData
     {
         RequestData::from(&self)
     }
-
+    pub fn set_state(&self, state: &str) -> bool
+    {
+        unsafe{
+            if let Ok(key) = CString::new("__RUST__STATE__")
+            {
+                if let Ok(value) = CString::new(state)
+                {
+                    let _ = dput(self.data, key.as_ptr() as *const c_char, libc::strdup(value.as_ptr() as *const c_char));
+                    return true;
+                }
+            }
+            
+            false
+        }
+    }
+    pub fn get_state(&self) -> Option<String>
+    {
+        let data = self.get_data();
+        match data.header.get("__RUST__STATE__")
+        {
+            Some(s) => Some(String::from(*s)),
+            None => None
+        }
+    }
     fn get_client(&self) -> *const Client
     {
        self.client
     }
-
+    pub fn port(&self) -> u32
+    {
+        unsafe
+        {
+            (*(*self.client).port_config).port
+        }
+    }
+    pub  fn htdocs(&self) ->Option<&str>
+    {
+        unsafe
+        {
+            if let Ok(s) = cstr!((*(*self.client).port_config).htdocs)
+            {
+                Some(s)
+            }
+            else
+            {
+                None
+            }
+        }
+   }
+   pub fn is_websocket(&self) -> bool
+   {
+        let data = self.get_data();
+        let header = data.get_header();
+        match header.get("__web_socket__")
+        {
+            Some(s) => true,
+            None => false
+        }
+   }
     pub fn read(&self, buf:&[u8]) -> Result<i32,i32>
     {
         unsafe
@@ -141,6 +210,71 @@ impl Request {
             else
             {
                 Err(ret)
+            }
+        }
+    }
+    /// This function is used only for web socket
+    ///
+    /// It read one frame data
+    pub fn wsread(&self, handle: &Fn(&[u8], bool)) -> WSStatus
+    {
+        unsafe{
+            if self.is_websocket()
+            {
+                let mut buf:Vec<u8> = vec![0; MAX_BUF_SIZE];
+                let header = ws_read_header(self.client);
+                if header != std::ptr::null()
+                {
+                    if (*header).mask == 0
+                    {
+                        libc::free(header as *mut c_void);
+                        ws_send_close(self.client,1011,0);
+                        return WSStatus::CLOSED;
+                    }
+                    // close opcode
+                    if (*header).opcode == 0x8
+                    {
+                        libc::free(header as *mut c_void);
+                        ws_send_close(self.client,1011,0);
+                        return WSStatus::CLOSED;
+                    }
+                    if (*header).opcode == 0x01 || (*header).opcode == 0x02
+                    {
+                        let bin = if (*header).opcode == 0x02 {true} else {false};
+                        
+                        loop
+                        {
+                            let n = ws_read_data(self.client, header, buf.len() as i32, buf.as_mut_ptr() as *const u8);
+                            if n == -1 
+                            {
+                                libc::free(header as *mut c_void);
+                                return return WSStatus::ERR;
+                            }
+                            if n == 0
+                            {
+                                libc::free(header as *mut c_void);
+                                return WSStatus::OK;
+                            }
+                            buf.truncate(n as usize);
+                            handle(&buf,bin);
+                            buf.resize(MAX_BUF_SIZE,0);
+                        }
+                    }
+                    else
+                    {
+                        libc::free(header as *mut c_void);
+                        ws_send_close(self.client,1011,0);
+                        return WSStatus::ERR;
+                    }
+                }
+                else
+                {
+                    WSStatus::ERR
+                }
+            }
+            else
+            {
+                WSStatus::NOT_WS
             }
         }
     }
@@ -164,7 +298,8 @@ pub struct Response
     header: HashMap<String, String>,
     client: *const Client,
     status: i32,
-    hflag:bool
+    hflag:bool,
+    ws: bool
 }
 
 impl Response{
@@ -177,13 +312,19 @@ impl Response{
             header: header,
             client:client,
             status: 200,
-            hflag: false
+            hflag: false,
+            ws: false
         }
     }
 
     pub fn set_status(&mut self, v:i32) 
     {
         self.status = v;
+    }
+
+    pub fn set_ws(&mut self, v:bool)
+    {
+        self.ws = v;
     }
 
     pub fn set_header(&mut self, k: & str, v: & str)
@@ -229,14 +370,32 @@ impl Response{
         self.send(b"\r\n\r\n" as &[u8])?;
         Ok(1)
     }
-    pub fn write(&mut self, buf:&[u8]) -> Result<i32,String>
+    pub fn write(&mut self, buf:&[u8], bin: bool) -> Result<i32,String>
     {
-        if !self.hflag 
+        if self.ws
         {
-            self.send_header()?;
-            self.set_flag(true);
+            unsafe{
+                if bin
+                {
+                    ws_send_text(self.client, buf.as_ptr() as *const c_char,0);
+                    Ok(buf.len() as i32)
+                }
+                else
+                {
+                    ws_send_binary(self.client, buf.as_ptr(), buf.len() as i32,0);
+                    Ok(buf.len() as i32)
+                }
+            }
         }
-        self.send(buf)
+        else
+        {
+            if !self.hflag 
+            {
+                self.send_header()?;
+                self.set_flag(true);
+            }
+            self.send(buf)
+        }
     }
     pub fn send(&self, buf:&[u8]) -> Result<i32,String>
     {
@@ -375,12 +534,13 @@ pub struct Task
 #[link(name="antd")]
 extern {
     fn antd_create_task(
-        handle: *const fn(*const c_void) -> c_void, 
+        handle: *const fn(*const c_void) -> *const c_void, 
         data: *const c_void,
-        callback:*const fn(*const c_void) -> c_void,
+        callback:*const fn(*const c_void) -> *const c_void,
         time: libc::time_t
     ) -> *const c_void;
     fn server_log(fmt: *const c_char, msg:*const c_char) -> ();
+    fn dput(dic:*const Dictionary, k: *const c_char, v: *const c_char) -> *const c_void;
     fn error_log(fmt: *const c_char, msg:*const c_char) -> ();
     fn antd_send(source: *const c_void, data: *const c_void, len: u32) -> i32;
     fn antd_recv(source: *const c_void,  data: *const c_void, len: u32) -> i32;
@@ -389,12 +549,13 @@ extern {
     fn antd_error(client:*const c_void, stat: i32, msg: *const c_char) -> ();
     fn get_status_str(stat: i32) -> *const c_char;
     // websocket
-    fn ws_read_header(client:*const c_void) -> *const WSHeader;
-    fn ws_send_text(client: *const c_void, data: *const c_char,mask: i32) -> ();
-    fn ws_send_file(client: *const c_void, data: *const c_char,mask: i32) -> ();
-    fn ws_send_binary(client:*const c_void, data: *const u8,mask:i32)->();
-    fn ws_send_close(client:*const c_void, stat:u32,mask:i32)->();
-    fn ws_read_data(client:*const c_void, header:*const WSHeader, size:i32, data:*const u8) -> i32;
+    fn ws_read_header(client:*const Client) -> *const WSHeader;
+    fn ws_send_text(client: *const Client, data: *const c_char,mask: i32) -> ();
+    fn ws_send_file(client: *const Client, data: *const c_char,mask: i32) -> ();
+    fn ws_send_binary(client:*const Client, data: *const u8, size: i32,mask:i32)->();
+    fn ws_send_close(client:*const Client, stat:u32,mask:i32)->();
+    fn ws_read_data(client:*const Client, header:*const WSHeader, size:i32, data:*const u8) -> i32;
+    fn server_time() -> *const c_char;
 }
 
 #[no_mangle]
@@ -402,6 +563,10 @@ pub unsafe extern fn handle(ptr: *const Request) -> *const c_void
 {
     let request = &*ptr;
     let mut response = Response::from(request.get_client());
+    if request.is_websocket()
+    {
+        response.set_ws(true);
+    }
     let task = process(&request, &mut response);
     task.get_ptr()
 }
@@ -434,7 +599,7 @@ pub fn log(prefix:&str, error:bool, args: Arguments<'_>)  {
 #[macro_export]
 macro_rules! LOG {
     ($($args:tt)*) => ({
-        let prefix = format!("[{}:{}]: ", file!(), line!());
+        let prefix = format!("{}: [{}:{}]: ", server_time_str(), file!(), line!());
         log(&prefix[..], false, format_args!($($args)*));
     })
 }
@@ -443,7 +608,7 @@ macro_rules! LOG {
 #[macro_export]
 macro_rules! ERROR {
     ($($args:tt)*) => ({ 
-        let prefix = format!("[{}:{}]: ", file!(), line!());
+        let prefix = format!("{}: [{}:{}]: ", server_time_str(), file!(), line!());
         log(&prefix[..], true, format_args!($($args)*));
     })
 }
@@ -456,12 +621,19 @@ impl Task {
             Task::from(std::ptr::null(), rq, std::ptr::null(),libc::time(&mut t))
         }
     }
-    pub fn from( handle: *const fn(*const Request) -> (),data: *const Request, callback:*const fn(*const Request) -> (), time: libc::time_t) -> Task
+    pub fn again(rq: *const Request) -> Task
+    {
+        let mut t: libc::time_t = 0;
+        unsafe{
+            Task::from(handle as *const fn(*const Request)-> *const c_void, rq, std::ptr::null(),libc::time(&mut t))
+        }
+    }
+    pub fn from( handle: *const fn(*const Request) -> *const c_void,data: *const Request, callback:*const fn(*const Request) -> *const c_void, time: libc::time_t) -> Task
     {
         unsafe{
             Task
             {
-                ptr: antd_create_task(handle as *const fn(*const c_void)-> c_void, data as *const c_void, callback as *const fn(*const c_void)-> c_void, time)
+                ptr: antd_create_task(handle as *const fn(*const c_void)->*const c_void, data as *const c_void, callback as *const fn(*const c_void)-> *const c_void, time)
             }
         }
     }
@@ -476,11 +648,9 @@ fn __(name: *const c_char, config: *const Config)
     unsafe{
         __PLUGIN__.name = libc::strdup(name);
         __PLUGIN__.dbpath = (*config).db_path;
-        __PLUGIN__.htdocs = (*config).htdocs;
+        __PLUGIN__.tmpdir = (*config).tmpdir;
         __PLUGIN__.pdir = (*config).plugins_dir;
-        __PLUGIN__.sport = (*config).port;
         __PLUGIN__.raw_body = 0;
-        __PLUGIN__.usessl = (*config).usessl;
     }
 }
 
@@ -499,9 +669,9 @@ pub extern  fn meta() -> *const PluginHeader
     }
 }
 
-pub fn plugin_name<'a>() -> Option<&'a str>
+pub fn tmpdir<'a>() -> Option<&'a str>
 {
-    if let Ok(name) = cstr!(__PLUGIN__.name)
+    if let Ok(name) = cstr!(__PLUGIN__.tmpdir)
     {
         Some(name)
     }
@@ -511,9 +681,9 @@ pub fn plugin_name<'a>() -> Option<&'a str>
     }
 }
 
-pub fn htdocs<'a>() -> Option<&'a str>
+pub fn plugin_name<'a>() -> Option<&'a str>
 {
-    if let Ok(name) = cstr!(__PLUGIN__.htdocs)
+    if let Ok(name) = cstr!(__PLUGIN__.name)
     {
         Some(name)
     }
@@ -547,11 +717,15 @@ pub fn db_root<'a>() -> Option<&'a str>
     }
 }
 
-pub fn port<'a>() -> u32
+pub fn server_time_str() -> String
 {
-    unsafe
+    if let Ok(s) = cstr!(server_time())
     {
-        __PLUGIN__.sport
+        String::from(s)
+    }
+    else
+    {
+        String::new()
     }
 }
 
